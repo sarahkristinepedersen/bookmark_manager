f79e9c5f42ff70b9e5544d8c319dc5f2e1223b14
Sarah Kristine Pedersen <mail@sarahkp.dk>
2015-09-22 15:49:49 +0100

    Everything before DatabaseCleaner

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+ruby '2.1.5'
+
+gem 'sinatra'
+
+group :test do
+  gem 'rspec'
+  gem 'rubocop-rspec'
+  gem 'rubocop'
+  gem 'coveralls', require: false
+  gem 'capybara'
+  gem 'byebug'
+  gem 'rspec-sinatra'
+  gem 'data_mapper'
+  gem 'dm-postgres-adapter'
+end
---
+++ app/app.rb
@@ -0,0 +1,20 @@
+require 'sinatra/base'
+
+class BookmarkManager < Sinatra::Base
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links/' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect to('/links')
+  end
+
+
+end
